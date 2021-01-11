c5e6f844b9edf21773788f7291dd3689119aba57
Posna <jomart17@ucm.es>
2021-01-12 00:02:19 +0100

    Coloritos de las particulas

--- Proyecto/Dark Baby Labyrinth/Assets/Scripts/TimeToLive.cs
+++ Proyecto/Dark Baby Labyrinth/Assets/Scripts/TimeToLive.cs
@@ -5,9 +5,38 @@ using UnityEngine;
 public class TimeToLive : MonoBehaviour
 {
     public float time_;
+    public float timeDying_;
 
     void Start()
     {
-        Destroy(gameObject, time_);
+        //Invoke("Die", time_);
+    }
+
+    void Update()
+    {
+        if(time_ <= 0)
+        {
+            if(timeDying_ <= 0)
+                Destroy(gameObject);
+            else
+                timeDying_ -= Time.deltaTime;
+        }
+        else
+        {
+            time_ -= Time.deltaTime;
+            if(time_ <= 0) GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+
+    public void set(float t, float td)
+    {
+        time_ = t;
+        timeDying_ = td;
+    }
+
+    void Die()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        Destroy(gameObject, timeDying_);
     }
 }
