74b85568cffe3fc5f4d189f3abbb91bde447032a
Posna <jomart17@ucm.es>
2021-01-13 17:03:13 +0100

    Menu y cambios de escenas

--- Proyecto/Dark Baby Labyrinth/Assets/Scripts/Baby.cs
+++ Proyecto/Dark Baby Labyrinth/Assets/Scripts/Baby.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Baby : SoundEmitter
 {
     public float soundCD;
+    public string scene;
     private float soundTimer;
+    private bool loading = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,15 @@ public class Baby : SoundEmitter
         {
             EmitSound(1.0f);
             soundTimer = soundCD;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "player" && !loading)
+        {
+            loading = true;
+            SceneManager.LoadScene(scene);
         }
     }
 }
---
+++ Proyecto/Dark Baby Labyrinth/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    
+    public void changeScene(string scene)
+    {
+        Debug.Log("Cambio de escena (se supone)");
+        SceneManager.LoadScene(scene);
+    }
+}
