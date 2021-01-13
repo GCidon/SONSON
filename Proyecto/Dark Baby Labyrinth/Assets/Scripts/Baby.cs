280ebc9e8b295b710d2162c8ef88e70257e7db2e
GCidon <38065027+GCidon@users.noreply.github.com>
2021-01-13 17:04:13 +0100

    Espectacular

--- Proyecto/Dark Baby Labyrinth/Assets/Scripts/Baby.cs
+++ Proyecto/Dark Baby Labyrinth/Assets/Scripts/Baby.cs
@@ -6,21 +6,46 @@ public class Baby : SoundEmitter
 {
     public float soundCD;
     private float soundTimer;
+    public GameObject player;
+    private float obsIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 dir = (player.transform.position - transform.position);
+        LayerMask mask = LayerMask.GetMask("Paredes");
+        if (Physics2D.Raycast(transform.position, dir, dir.magnitude, mask)) obstruccion();
+        else desobstruccion();
+
         soundTimer -= Time.deltaTime;
 
         if (soundTimer <= 0)
         {
             EmitSound(1.0f);
             soundTimer = soundCD;
+        }
+
+        GetComponent<FMODUnity.StudioEventEmitter>().SetParameter("Obstruccion", obsIndex);
+    }
+
+    private void obstruccion() { 
+        if(obsIndex < 0.7f)
+        {
+            obsIndex += 0.01f;
+        }
+    }
+
+    private void desobstruccion()
+    {
+        if (obsIndex > 0.0f)
+        {
+            obsIndex -= 0.01f;
         }
     }
 }
