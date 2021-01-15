0c4c5e776f515b59a703bf5e13d2048817eead03
Posna <jomart17@ucm.es>
2021-01-15 18:43:53 +0100

    Sonidos de trafico

---
+++ Proyecto/Dark Baby Labyrinth/Assets/Scripts/Altavoz.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altavoz : SoundEmitter
+{
+    
+    public float soundCD;
+
+    void Start()
+    {
+        InvokeRepeating("EmitSoundWithColors", 0.0f, soundCD);
+    }
+
+    void EmitSoundWithColors()
+    {
+        Color randomColor = new Color(
+            Random.Range(0f, 1f), //Red
+            Random.Range(0f, 1f), //Green
+            Random.Range(0f, 1f), //Blue
+            1 //Alpha (transparency)
+        );
+        setColor(randomColor);
+        EmitSound(1.0f);
+    }
+}
---
+++ Proyecto/Dark Baby Labyrinth/Assets/Scripts/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    
+    public int dir;
+    public float speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed*dir);
+    }
+}
